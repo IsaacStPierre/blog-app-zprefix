4bdfc46e3006198156d16a769e5cd3650d3b2402
IsaacStPierre <isaacstpierre@gmail.com>
2022-07-02 00:25:50 -0600

    testing for deployment

--- api/src/index.js
+++ api/src/index.js
@@ -1,6 +1,11 @@
 const app = require('./app');
 const PORT = process.env.PORT || 8082;
 
+
+app.get('/', (request, response) => {
+    response.send(`Working :)`)
+})
+
 app.listen(PORT, () => {
     console.log(`Capstone application listening on ${PORT}`);
 })
